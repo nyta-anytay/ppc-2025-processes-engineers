dd81a41b8406d58ab286c3e02862013a216db659
ann lazareva <nyutik05@gmail.com>
2025-12-19 00:20:44 +0300

    Add torus grid solution f

--- tasks/lazareva_a_torus_grid/tests/functional/main.cpp
+++ tasks/lazareva_a_torus_grid/tests/functional/main.cpp
@@ -6,6 +6,7 @@
 #include <cstddef>
 #include <string>
 #include <tuple>
+#include <cmath> 
 
 #include "lazareva_a_torus_grid/common/include/common.hpp"
 #include "lazareva_a_torus_grid/mpi/include/ops_mpi.hpp"
